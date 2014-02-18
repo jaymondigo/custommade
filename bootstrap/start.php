15b25416cf94969b3a4aab5673c53b731d8ad5cd
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2014-02-18 19:22:38 +0800

    added fox host to startphp

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('alenteria-PC', 'devf-ubuntu'),
+	'local' => array('alenteria-PC', 'devf-ubuntu','fox'),
 	'staging' => array('staging.custommade.com'),
 	'production' => array('custommade.com')
 
